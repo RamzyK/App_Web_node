fa7e3066c22fdfb08a620398e92f749f5e579de8
Ramzy <gkermad.ramzy@gmail.com>
2019-03-13 15:12:04 +0100

    map geocode

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node App' });
 });
 
 router.post('/ville', function(req, res) {
-  res.render('ville', {ville: req.body.nom_ville, description: req.body.desc})
+  axios.get("https://geocode.xyz/" + req.body.nom_ville + "?json=1&auth=218622879073471220010x1964")
+    .then(function (response) {
+      var long = response.data.longt;
+      var lat = response.data.latt;
+      console.log("Longitude: " + long + "\n");
+      console.log("Latitude: " + lat + "\n");
+      res.render('ville', {ville: req.body.nom_ville, description: req.body.desc, longitude: long, latitude: lat})
+    })
+    .catch(function (error) {
+      console.log("Error: " + error + "\n\n\n");
+      res.send(error);
+    });
+
 })
+
+
 
 module.exports = router;
