b1ff67fcd76d35e996e22697638891b246a02d14
Ramzy <gkermad.ramzy@gmail.com>
2019-03-14 21:55:15 +0100

    Bonus nock

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 
+
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Node App' });
 });
@@ -9,17 +11,11 @@ router.get('/', function(req, res, next) {
 router.post('/ville', function(req, res) {
   axios.get("https://geocode.xyz/" + req.body.nom_ville + "?json=1&auth=218622879073471220010x1964")
     .then(function (response) {
-      const nomVille = '';
-      if(response.body.error == null || response.body.error == undefined){
-        nomVille = "La ville recherchée n'existe pas !"
-      }else{
-        nomVille = response.body.error;
-      }
       var long = response.data.longt;
       var lat = response.data.latt;
       console.log("Longitude: " + long + "\n");
       console.log("Latitude: " + lat + "\n");
-      res.render('ville', {ville: nomVille, description: req.body.desc, longitude: long, latitude: lat})
+      res.render('ville', {ville: req.body.nom_ville, description: req.body.desc, longitude: long, latitude: lat})
     })
     .catch(function (error) {
       console.log("Error: " + error + "\n\n\n");
--- test.js
+++ test.js
@@ -1,6 +1,28 @@
 import test from 'ava'
 const nock = require('nock')
 var axios = require('axios');
+var express = require('express');
+const port = 3000;
+const app = require('./app.js');
+const indexRouter = require('./routes/index.js');
+app.use('/ville', indexRouter);
+
+app.listen(port, () => {
+  test('Test ville nock', async t => {
+    try {
+      const city = 'berlin';
+      nock('https://geocode.xyz')
+      .get('/' + city + '?json=1&auth=218622879073471220010x1964')
+      .reply(200, { longt: 290, latt: 100});
+      const { data } = await axios.post('http://localhost:3000/ville', { nom_ville: city });
+      console.log(data);
+    }
+    catch (error) {
+      console.log('error');
+    }
+    t.pass();
+  });
+});
 
 test("La page d'index du site comprend bien un formulaire HTML", async t => {
     try {
@@ -17,23 +39,11 @@ test("La page d'index du site comprend bien un formulaire HTML", async t => {
 
 test('Le nom de la ville correspond bien au nom passé en paramètre', async t => {
     try {
-      const responseGet = await axios.post('https://cryptic-basin-35008.herokuapp.com/ville', {
+      const responseGet = await axios.post('http://localhost:3000/ville', {
         nom_ville: "Paris"
       });
 
       t.is(responseGet.data.toString().includes("Paris"), true);
-    } catch (e) {
-      t.fail("Erreur serveur!")
-    }
-});
-
-test('Le nom de la ville correspond bien au nom passé en paramètre', async t => {
-    try {
-      const responseGet = await axios.post('https://cryptic-basin-35008.herokuapp.com/ville', {
-        nom_ville: "AZERTYUIO"
-      });
-
-      t.is(responseGet.data.toString().includes("La ville recherchée n'existe pas !"), true);
     } catch (e) {
       t.fail("Erreur serveur!")
     }
